36f3daa23b718e6fb8726f3a79b450ed0a13ad88
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 14:37:23 -0300

    Uri 2020 Idade em dias

---
+++ IdadeEmDias/IdadeEmDias/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace IdadeEmDias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, ANOS, MESES, DIAS, RESTO;
+
+            N = int.Parse(Console.ReadLine());
+
+            ANOS = N / 365;
+            RESTO = N % 365;
+            MESES = RESTO / 30;
+            DIAS = RESTO % 30;
+
+            Console.WriteLine(ANOS + " ano(s)");
+            Console.WriteLine(MESES + " mes(es)");
+            Console.WriteLine(DIAS + " dia(s)");           
+        }
+    }
+}
